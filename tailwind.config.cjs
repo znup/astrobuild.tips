5848e94abf2bb79b9c2e412101eaaba5ea60fd76
SirAdan <lpelcastre@bomuca.com>
2023-09-11 15:25:11 -0700

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  // darkMode: 'class',
+  content: [
+    // './src/pages/index.astro', // Ruta a tu archivo HTML principal
+    './src/**/*/*.{astro,html,js,jsx}',
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
