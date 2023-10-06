ee828cf4e9e6d910c6c065d6bbbdf01e7f32ab6a
SirAdan <lpelcastre@bomuca.com>
2023-10-06 13:17:12 -0700

    [ADD] BaseHeader / [ADD] rss / [UPDATE] HeaderTop / [UPDATE] Base.astro

--- src/components/HeaderTop.tsx
+++ src/components/HeaderTop.tsx
@@ -119,7 +119,7 @@ export default function Example() {
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-slate-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-slate-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-800/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
@@ -139,12 +139,12 @@ export default function Example() {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
+            <div className="-my-6 divide-y divide-gray-500/10 dark:divide-gray-200/10">
               <div className="space-y-2 py-6">
                 <Disclosure as="div" className="-mx-3">
                   {({ open }) => (
                     <>
-                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-gray-200">
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-gray-200 sm:hover:dark:text-red-600">
                         Product
                         <ChevronDownIcon
                           className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
@@ -157,7 +157,7 @@ export default function Example() {
                             key={item.name}
                             as="a"
                             href={item.href}
-                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50 hover:dark:text-gray-900"
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 lg:text-gray-900 hover:bg-gray-50 lg:hover:bg-slate-800 lg:hover:dark:text-gray-900 sm:text-gray-200"
                           >
                             {item.name}
                           </Disclosure.Button>
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,17 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const posts = await getCollection('tips');
+  return rss ({
+    title: 'Astro Tips',
+    description: 'My Astro Page',
+    site: context.site,
+    items: posts.map((post) => {
+      return {
+        ...post.data,
+        link: `/posts/${post.slug}/`,
+      }
+    }),
+  });
+}
